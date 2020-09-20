1f8bc2266405e76c50bb2244788d6c9bb93fa2b4
abeidahmed <abeidahmed92@gmail.com>
2020-09-20 16:56:11 +0530

    install create-react-app and configure

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-indigo-500">hello world</h1>
+    </div>
+  );
+}
+
+export default App;
