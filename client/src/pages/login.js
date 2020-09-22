40eb77cca9aa39174ca5610d610faacb3266bd53
abeidahmed <abeidahmed92@gmail.com>
2020-09-22 14:23:54 +0530

    fix logo fill on login and signup page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -89,7 +89,12 @@ export default connect(null, mapDispatchToProps)(Login);
 function LoginHeader() {
   return (
     <div className="px-4">
-      <Icon icon="twitter-solid" className="w-12 h-12 mx-auto text-blue-500" />
+      <Icon
+        icon="twitter-solid"
+        stroke="none"
+        fill="currentColor"
+        className="w-12 h-12 mx-auto text-blue-500"
+      />
       <h2 className="mt-6 text-3xl font-extrabold leading-9 text-center text-gray-900">
         Sign in to your account
       </h2>
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -100,7 +100,12 @@ export default connect(null, mapDispatchToProps)(Signup);
 function SignupHeader() {
   return (
     <div className="px-4">
-      <Icon icon="twitter-solid" className="w-12 h-12 mx-auto text-blue-500" />
+      <Icon
+        icon="twitter-solid"
+        stroke="none"
+        fill="currentColor"
+        className="w-12 h-12 mx-auto text-blue-500"
+      />
       <h2 className="mt-6 text-3xl font-extrabold leading-9 text-center text-gray-900">
         See what's happening in the world right now
       </h2>
