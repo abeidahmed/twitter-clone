10a05bad0cd0b69728e84b372cc0c778d1aed106
abeidahmed <abeidahmed92@gmail.com>
2020-09-23 19:08:02 +0530

    add active class to tabl

--- client/src/components/tab.js
+++ client/src/components/tab.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export function Tab({ links }) {
   return (
-    <nav className="flex items-center justify-between min-w-0 mt-3 -mb-1">
+    <nav className="flex items-center justify-between min-w-0 mt-3">
       {links.map(({ title, path }) => (
-        <Link
+        <NavLink
           key={title}
+          exact
           to={path}
-          className="block w-full px-2 py-3 text-sm font-medium text-center text-gray-600 truncate whitespace-no-wrap transition duration-150 ease-in-out border-b-2 border-transparent sm:text-base hover:bg-blue-50 hover:text-blue-600"
+          activeClassName="border-blue-600 text-blue-600"
+          className="block w-full px-2 py-3 text-sm font-medium text-center text-gray-500 truncate whitespace-no-wrap transition duration-150 ease-in-out border-b-2 border-transparent focus:outline-none focus:border-blue-600 sm:text-base hover:bg-blue-50 hover:text-blue-600"
         >
           {title}
-        </Link>
+        </NavLink>
       ))}
     </nav>
   );
