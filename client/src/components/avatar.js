7609a4fbe5ab66c15506b480abfec2f14d45f49f
abeidahmed <abeidahmed92@gmail.com>
2020-09-26 00:24:28 +0530

    show gray bg when avatar is empty

--- client/src/components/avatar.js
+++ client/src/components/avatar.js
@@ -2,6 +2,13 @@ import React from 'react';
 import cn from 'classnames';
 
 export function Avatar({ src, alt, size, variant, ...props }) {
+  const wrapperClass = cn([
+    'bg-gray-300',
+    {
+      'border-4 border-white': variant === 'bordered',
+    },
+  ]);
+
   const avatarClass = cn([
     'flex-shrink-0 rounded-full',
     {
@@ -9,9 +16,12 @@ export function Avatar({ src, alt, size, variant, ...props }) {
       'w-10 h-10': size === 'md',
       'w-12 h-12': size === 'lg',
       'w-20 h-20 lg:w-32 lg:h-32': size === 'xl',
-      'border-4 border-white': variant === 'bordered',
     },
   ]);
 
-  return <img src={src} alt={alt} className={avatarClass} />;
+  return (
+    <div className={`${avatarClass} ${wrapperClass}`}>
+      {src && <img src={src} alt={alt} className={avatarClass} {...props} />}
+    </div>
+  );
 }
