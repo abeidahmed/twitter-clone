f350a1ba56e92ad1e3314e462ce5253dbeb2ee17
abeidahmed <abeidahmed92@gmail.com>
2020-09-26 16:32:54 +0530

    make search req to api

--- client/src/api/all-users.js
+++ client/src/api/all-users.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
+import qs from 'query-string';
 import { header } from 'middleware/header';
 
-export async function allUsers() {
-  return await axios.get('/users', header());
+export async function allUsers(key, { q }) {
+  const url = qs.stringifyUrl(
+    {
+      url: '/users',
+      query: {
+        q,
+      },
+    },
+    { skipEmptyString: true }
+  );
+
+  return await axios.get(url, header());
 }
--- client/src/pages/explore.js
+++ client/src/pages/explore.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import { useSetTitle } from 'store/page-title';
-import UserCard from 'components/user-card';
+import { useDebounce } from 'hooks/debounce';
 import { allUsers } from 'api/all-users';
+import UserCard from 'components/user-card';
 import { Spinner } from 'components/spinner';
 import { SearchBar } from 'components/search-bar';
 
 function Explore() {
   useSetTitle('Explore', null);
 
-  const { data, isLoading, isError } = useQuery('allUsers', allUsers);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  if (isLoading || isError) return <Spinner />;
+  const debouncedSearchTerm = useDebounce(searchTerm);
+  const { data, isLoading, isError } = useQuery(
+    ['allUsers', { q: debouncedSearchTerm }],
+    allUsers
+  );
 
   return (
     <main className="py-3">
       <div className="px-4 mt-3 mb-6">
-        <SearchBar placeholder="Explore people" />
+        <SearchBar
+          placeholder="Explore people"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       </div>
-      <section className="w-full">
-        {data.data.users.map((user) => (
-          <UserCard key={user.id} user={user} />
-        ))}
-      </section>
+      <div className="relative">
+        {isLoading || isError ? (
+          <Spinner />
+        ) : (
+          <section className="w-full">
+            {data.data.users.map((user) => (
+              <UserCard key={user.id} user={user} />
+            ))}
+          </section>
+        )}
+      </div>
     </main>
   );
 }
