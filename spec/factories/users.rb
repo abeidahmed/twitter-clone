5573d67e4f30e4561d3b7cd674b1078680d0b0a9
abeidahmed <abeidahmed92@gmail.com>
2020-09-26 20:35:27 +0530

    change to single quotes

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,9 +3,9 @@ FactoryBot.define do
     sequence(:email) { |n| "user#{n}@example.com" }
     sequence(:twitter_handle) { |n| "username#{n}" }
     sequence(:name) { |n| "name#{n}" }
-    bio { "lorem ipsum color de matur" }
-    location { "India" }
-    website { "https://localhost:3000" }
-    password { "mamakane" }
+    bio { 'lorem ipsum color de matur' }
+    location { 'India' }
+    website { 'https://localhost:3000' }
+    password { 'mamakane' }
   end
 end
