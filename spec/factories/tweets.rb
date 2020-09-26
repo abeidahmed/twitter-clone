f7d8aba7254f50f57a8b3de8c29abb6d790d4345
abeidahmed <abeidahmed92@gmail.com>
2020-09-26 20:35:43 +0530

    add reply_status trait

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :tweet do
-    body { "MyText" }
-    image { "MyString" }
-    user { nil }
+    body { 'hello world' }
+    image { 'imageurl' }
+    reply_status { 'everyone' }
+    user factory: :user
+
+    trait :people_you_know do
+      reply_status { 'people_you_know' }
+    end
   end
 end
