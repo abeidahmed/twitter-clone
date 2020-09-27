db5797f9fdc60b8b6d3a8a67aff5d75cd8d29695
abeidahmed <abeidahmed92@gmail.com>
2020-09-27 10:06:38 +0530

    make factory association

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :tweet do
+    association :user, factory: :user
     body { 'hello world' }
     image { Rails.root.join('spec', 'factories', 'test_image', 'image.jpg') }
     reply_status { 'everyone' }
-    user factory: :user
 
     trait :people_you_know do
       reply_status { 'people_you_know' }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,5 +7,16 @@ FactoryBot.define do
     location { 'India' }
     website { 'https://localhost:3000' }
     password { 'mamakane' }
+
+    factory :user_with_tweets do
+      transient do
+        tweets_count { 1 }
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:tweet, evaluator.tweets_count, user: user)
+        user.reload
+      end
+    end
   end
 end
