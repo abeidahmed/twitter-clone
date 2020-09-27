012a5689efc1a6f87400f3985b46759ad6b2efe5
abeidahmed <abeidahmed92@gmail.com>
2020-09-27 14:17:52 +0530

    add more variant to file-upload component

--- client/src/components/file-upload.js
+++ client/src/components/file-upload.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import cn from 'classnames';
 import { Icon } from './icon';
 
-export function FileUpload({ ...props }) {
+export function FileUpload({ size, color, icon, ...props }) {
+  const uploadClass = cn([
+    'p-2 transition duration-150 ease-in-out rounded-full cursor-pointer focus:outline-none',
+    {
+      'text-white hover:bg-gray-600 focus:shadow-outline-gray':
+        color === 'overlay',
+      'text-blue-500 hover:bg-blue-50 focus:shadow-outline-blue':
+        color === 'white',
+    },
+  ]);
+
+  const uploadIconClass = cn([
+    'block',
+    {
+      'w-5 h-5': size === 'sm',
+      'w-6 h-6': size === 'md',
+    },
+  ]);
+
   return (
-    <label className="p-2 transition duration-150 ease-in-out rounded-full cursor-pointer focus:outline-none focus:shadow-outline-gray hover:bg-gray-600">
+    <label className={uploadClass}>
       <input type="file" className="hidden" {...props} />
-      <Icon className="w-5 h-5 text-white" icon="camera" />
+      <Icon className={uploadIconClass} icon={icon} />
     </label>
   );
 }
