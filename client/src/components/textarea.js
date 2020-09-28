b739d0bcf24dcd1a5cabb290d20ad26112017686
abeidahmed <abeidahmed92@gmail.com>
2020-09-28 12:07:55 +0530

    udpate textarea component

--- client/src/components/textarea.js
+++ client/src/components/textarea.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import cn from 'classnames';
 import { fieldValidation } from 'utils/helpers';
 
 export function Textarea({ id, label, error, errorType, resize, ...props }) {
+  const textareaClass = cn([
+    'block w-full mt-1 shadow-sm form-textarea',
+    {
+      'resize-none': !resize,
+    },
+  ]);
+
   return (
     <div>
       <label htmlFor={id} className="block text-sm font-medium text-gray-700">
         {label}
       </label>
-      <textarea
-        id={id}
-        {...props}
-        className={`${
-          !resize && 'resize-none'
-        } block w-full mt-1 shadow-sm form-textarea`}
-      />
+      <textarea id={id} {...props} className={textareaClass} />
       {fieldValidation(error, errorType) && (
         <p className="mt-1 text-sm font-medium text-red-700">
           {fieldValidation(error, errorType)}
