db8c16f3a119a29725efdb727af55495b1bd0a5a
abeidahmed <abeidahmed92@gmail.com>
2020-09-29 14:25:00 +0530

    pass in null param

--- client/src/api/vote-tweet.js
+++ client/src/api/vote-tweet.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 import { header } from 'middleware/header';
 
 export async function voteTweet({ id }) {
-  return await axios.post(`/tweets/${id}/vote`, header());
+  return await axios.post(`/tweets/${id}/vote`, null, header());
 }
