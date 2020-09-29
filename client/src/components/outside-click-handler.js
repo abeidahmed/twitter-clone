fd4a2a511d79cb0a560c4942881ee119bf45fab9
abeidahmed <abeidahmed92@gmail.com>
2020-09-29 21:14:19 +0530

    make outside-click-handler component

---
+++ client/src/components/outside-click-handler.js
@@ -0,0 +1,39 @@
+import React, { useCallback, useRef, useEffect } from 'react';
+
+export function OutsideClickHandler({ onOutsideClick, children, ...props }) {
+  const containerRef = useRef(null);
+
+  const escapeListener = useCallback(
+    (e) => {
+      if (e.key === 'Escape') {
+        onOutsideClick();
+      }
+    },
+    [onOutsideClick]
+  );
+
+  const clickListener = useCallback(
+    (e) => {
+      if (!containerRef.current.contains(e.target)) {
+        onOutsideClick();
+      }
+    },
+    [onOutsideClick]
+  );
+
+  useEffect(() => {
+    document.addEventListener('click', clickListener);
+    document.addEventListener('keyup', escapeListener);
+
+    return () => {
+      document.removeEventListener('click', clickListener);
+      document.removeEventListener('keyup', escapeListener);
+    };
+  }, [clickListener, escapeListener]);
+
+  return (
+    <div ref={containerRef} {...props}>
+      {children}
+    </div>
+  );
+}
