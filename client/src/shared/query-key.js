7330a984004140c6e713a9a7f30705f8452073fe
abeidahmed <abeidahmed92@gmail.com>
2020-09-29 11:27:42 +0530

    make show user constant

--- client/src/shared/query-key.js
+++ client/src/shared/query-key.js
@@ -3,3 +3,4 @@ export const ALL_FOLLOWERS = 'ALL_FOLLOWERS';
 export const ALL_TWEETS = 'ALL_TWEETS';
 export const ALL_USERS = 'ALL_USERS';
 export const SHOW_USER = 'SHOW_USER';
+export const SHOW_TWEET = 'SHOW_TWEET';
