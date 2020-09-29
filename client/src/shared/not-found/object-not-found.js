1cce4b976da58d015f1eeb6f4e1a97a78c3d0ed1
abeidahmed <abeidahmed92@gmail.com>
2020-09-29 17:23:57 +0530

    add generalized not-found object component

---
+++ client/src/shared/not-found/object-not-found.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Icon } from 'components/icon';
+
+function ObjectNotFound({ description }) {
+  return (
+    <section className="p-4">
+      <div className="w-full max-w-md mx-auto">
+        <Icon icon="emoji-sad" className="w-10 h-10 mx-auto text-gray-300" />
+        <h2 className="mt-4 text-center text-gray-700">{description}</h2>
+      </div>
+    </section>
+  );
+}
+
+export default ObjectNotFound;
