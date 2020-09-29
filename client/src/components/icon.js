73de034484c20356ecc5aff32751f6f9df127ec9
abeidahmed <abeidahmed92@gmail.com>
2020-09-29 17:24:17 +0530

    add emoji-sad icon

--- client/src/components/icon.js
+++ client/src/components/icon.js
@@ -91,6 +91,17 @@ const Glyph = ({ icon }) => {
           />
         </g>
       );
+    case 'emoji-sad':
+      return (
+        <g>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </g>
+      );
     case 'hashtag':
       return (
         <g>
