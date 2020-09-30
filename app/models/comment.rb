a795a9333c1bfd8580dfa21752f4e27138f873b9
abeidahmed <abeidahmed92@gmail.com>
2020-09-30 10:33:56 +0530

    add comment content validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+
+  validates_length_of :content, maximum: 240
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  describe 'validations' do
+    it { should validate_length_of(:content).is_at_most(240) }
+  end
+
   describe 'associations' do
     it { should belong_to(:user) }
 
