9dad2fb2c11da646d1b4c38bc932d601b77fc09b
abeidahmed <abeidahmed92@gmail.com>
2020-10-01 21:35:48 +0530

    add all user tweets query key

--- client/src/shared/query-key.js
+++ client/src/shared/query-key.js
@@ -2,6 +2,7 @@ export const ALL_FOLLOWINGS = 'ALL_FOLLOWINGS';
 export const ALL_FOLLOWERS = 'ALL_FOLLOWERS';
 export const ALL_TWEETS = 'ALL_TWEETS';
 export const ALL_USERS = 'ALL_USERS';
+export const ALL_USER_TWEETS = 'ALL_USER_TWEETS';
 export const SHOW_USER = 'SHOW_USER';
 export const SHOW_TWEET = 'SHOW_TWEET';
 export const ALL_TWEET_LIKERS = 'ALL_TWEET_LIKERS';
