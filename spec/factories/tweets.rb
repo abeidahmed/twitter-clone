a7c982268dcfcf04c50668377d0ec6764c6a809c
abeidahmed <abeidahmed92@gmail.com>
2020-10-01 15:40:01 +0530

    add tweets with comments factory

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -9,5 +9,16 @@ FactoryBot.define do
     trait :people_you_know do
       reply_status { 'people_you_know' }
     end
+
+    factory :tweet_with_comments do
+      transient do
+        comments_count { 1 }
+      end
+
+      after(:create) do |tweet, evaluator|
+        create_list(:comment, evaluator.comments_count, tweet: tweet)
+        tweet.reload
+      end
+    end
   end
 end
