cd5c31df32be27473a7d37d6c9a3bb6b6f2e733b
abeidahmed <abeidahmed92@gmail.com>
2020-10-01 21:33:35 +0530

    add fetch all tweets with respect to user api

--- client/src/api/all-tweets.js
+++ client/src/api/all-tweets.js
@@ -1,6 +1,21 @@
 import axios from 'axios';
+import qs from 'query-string';
 import { header } from 'middleware/header';
 
 export async function allTweets(key) {
   return await axios.get('/tweets', header());
+}
+
+export async function allUserTweets(key, { id, filter = '' }) {
+  const url = qs.stringifyUrl(
+    {
+      url: `/users/${id}/tweets`,
+      query: {
+        filter,
+      },
+    },
+    { skipEmptyString: true }
+  );
+
+  return await axios.get(url, header());
 }
