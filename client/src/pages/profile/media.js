4764f0387222da41169b086d7b1633aeb986a0c3
abeidahmed <abeidahmed92@gmail.com>
2020-10-01 21:45:52 +0530

    show user tweets with only images

--- client/src/pages/profile/media.js
+++ client/src/pages/profile/media.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useSetTitle } from 'store/page-title';
+import { allUserTweets } from 'api/all-tweets';
+import * as a from 'shared/user-defaults';
+import * as q from 'shared/query-key';
+import { TwitterCard } from 'components/twitter-card';
+import { Spinner } from 'components/spinner';
 
-function Media() {
+function Media({ user }) {
+  useSetTitle(user.name || a.DEFAULT_NAME, `@${user.twitterHandle}`);
+  const { twitterHandle } = user;
+
+  const { data: { data: { tweets } = {} } = {}, isLoading, isError } = useQuery(
+    [q.ALL_USER_TWEETS_MEDIA, { id: twitterHandle, filter: 'media' }],
+    allUserTweets
+  );
+
   return (
-    <div>
-      <h1>hello world from media</h1>
+    <div className="relative">
+      <section>
+        {isLoading || isError ? (
+          <Spinner />
+        ) : (
+          tweets.map((tweet) => (
+            <TwitterCard key={tweet.id} tweet={tweet} user={user} />
+          ))
+        )}
+      </section>
     </div>
   );
 }
