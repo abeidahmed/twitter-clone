6d08314a6ddd298ea4d665bdd91dd6af819cf325
abeidahmed <abeidahmed92@gmail.com>
2020-10-01 23:38:37 +0530

    change api end-point to user media tweets

--- client/src/api/all-tweets.js
+++ client/src/api/all-tweets.js
@@ -1,23 +1,12 @@
 import axios from 'axios';
-import qs from 'query-string';
 import { header } from 'middleware/header';
 
 export async function allTweets(key) {
   return await axios.get('/tweets', header());
 }
 
-export async function allUserTweets(key, { id, filter = '' }) {
-  const url = qs.stringifyUrl(
-    {
-      url: `/users/${id}/tweets`,
-      query: {
-        filter,
-      },
-    },
-    { skipEmptyString: true }
-  );
-
-  return await axios.get(url, header());
+export async function allUserMediaTweets(key, { id }) {
+  return await axios.get(`/users/${id}/media_tweets`, header());
 }
 
 export async function allUserLikedTweets(key, { id }) {
