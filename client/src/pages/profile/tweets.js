6535a37f86930cc1f571522ba852f87b0948e582
abeidahmed <abeidahmed92@gmail.com>
2020-10-01 21:36:05 +0530

    show user tweets

--- client/src/pages/profile/tweets.js
+++ client/src/pages/profile/tweets.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useSetTitle } from 'store/page-title';
+import { allUserTweets } from 'api/all-tweets';
 import * as a from 'shared/user-defaults';
+import * as q from 'shared/query-key';
 import { TwitterCard } from 'components/twitter-card';
+import { Spinner } from 'components/spinner';
 
 function Tweets({ user }) {
   useSetTitle(user.name || a.DEFAULT_NAME, `@${user.twitterHandle}`);
-  const { tweets } = user;
+  const { twitterHandle } = user;
+
+  const { data: { data: { tweets } = {} } = {}, isLoading, isError } = useQuery(
+    [q.ALL_USER_TWEETS, { id: twitterHandle }],
+    allUserTweets
+  );
 
   return (
     <div className="relative">
       <section>
-        {tweets.map((tweet) => (
-          <TwitterCard key={tweet.id} tweet={tweet} user={user} />
-        ))}
+        {isLoading || isError ? (
+          <Spinner />
+        ) : (
+          tweets.map((tweet) => (
+            <TwitterCard key={tweet.id} tweet={tweet} user={user} />
+          ))
+        )}
       </section>
     </div>
   );
