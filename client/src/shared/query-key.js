fb25f73bb3d11392e8e05bbd7a5af024404ddbdb
abeidahmed <abeidahmed92@gmail.com>
2020-10-01 23:15:17 +0530

    add user liked tweets query key

--- client/src/shared/query-key.js
+++ client/src/shared/query-key.js
@@ -4,6 +4,7 @@ export const ALL_TWEETS = 'ALL_TWEETS';
 export const ALL_USERS = 'ALL_USERS';
 export const ALL_USER_TWEETS = 'ALL_USER_TWEETS';
 export const ALL_USER_TWEETS_MEDIA = 'ALL_USER_TWEETS_MEDIA';
+export const USER_LIKED_TWEETS = 'USER_LIKED_TWEETS';
 export const SHOW_USER = 'SHOW_USER';
 export const SHOW_TWEET = 'SHOW_TWEET';
 export const ALL_TWEET_LIKERS = 'ALL_TWEET_LIKERS';
