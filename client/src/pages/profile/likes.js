4754771ff16ad2fe90e1e41061f76fecae38fbd9
abeidahmed <abeidahmed92@gmail.com>
2020-10-01 23:16:25 +0530

    list all user-liked-tweets

--- client/src/pages/profile/likes.js
+++ client/src/pages/profile/likes.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useSetTitle } from 'store/page-title';
+import { allUserLikedTweets } from 'api/all-tweets';
+import * as a from 'shared/user-defaults';
+import * as q from 'shared/query-key';
+import { TwitterCard } from 'components/twitter-card';
+import { Spinner } from 'components/spinner';
 
-function Likes() {
+function Likes({ user }) {
+  useSetTitle(user.name || a.DEFAULT_NAME, `@${user.twitterHandle}`);
+  const { twitterHandle } = user;
+
+  const { data: { data: { tweets } = {} } = {}, isLoading, isError } = useQuery(
+    [q.USER_LIKED_TWEETS, { id: twitterHandle }],
+    allUserLikedTweets
+  );
+
   return (
-    <div>
-      <h1>hello from likes</h1>
+    <div className="relative">
+      <section>
+        {isLoading || isError ? (
+          <Spinner />
+        ) : (
+          tweets.map((tweet) => (
+            <TwitterCard key={tweet.id} tweet={tweet} user={tweet.user} />
+          ))
+        )}
+      </section>
     </div>
   );
 }
