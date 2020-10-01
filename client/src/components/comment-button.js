961d2dbd8eac4f535775345b66c86ced08bdbf06
abeidahmed <abeidahmed92@gmail.com>
2020-10-01 11:14:47 +0530

    remove logic from component to make it more reusable

--- client/src/components/comment-button.js
+++ client/src/components/comment-button.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import cn from 'classnames';
-import { useModalType } from 'store/modal';
 import { Icon } from './icon';
 
 export function CommentButton({ tweet, showCount, count, size, ...props }) {
-  const { modalOn, types } = useModalType();
-  const {
-    tweetID,
-    twitterName,
-    twitterTwitterHandle,
-    twitterAvatar,
-    tweetBody,
-    tweetCreatedAt,
-  } = tweet;
-
   const iconClass = cn([
     'rounded-full',
     {
@@ -22,25 +11,10 @@ export function CommentButton({ tweet, showCount, count, size, ...props }) {
     },
   ]);
 
-  function handleComment() {
-    modalOn({
-      modalType: types.CREATE_COMMENT_ON_COMMENT,
-      modalProps: {
-        tweetID,
-        twitterName,
-        twitterTwitterHandle,
-        twitterAvatar,
-        tweetBody,
-        tweetCreatedAt,
-      },
-    });
-  }
-
   return (
     <button
       className="relative inline-flex items-center text-sm text-gray-500 group focus:outline-none hover:text-blue-500 focus:text-blue-500"
       {...props}
-      onClick={handleComment}
     >
       <i className="p-2 rounded-full group-hover:bg-blue-50 group-focus:bg-blue-50">
         <Icon icon="chat" className={iconClass} />
