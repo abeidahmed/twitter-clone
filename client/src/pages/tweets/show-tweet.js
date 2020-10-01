f1e2cea994903210d88511e1bc98730830413057
abeidahmed <abeidahmed92@gmail.com>
2020-10-01 11:14:19 +0530

    isolate comment button logic and show comments

--- client/src/pages/tweets/show-tweet.js
+++ client/src/pages/tweets/show-tweet.js
@@ -16,11 +16,13 @@ import { TweetCardOption } from 'components/tweet-card-option';
 import { LikeButton } from 'components/like-button';
 import { TweetStats } from 'components/tweet-stats';
 import { CommentButton } from 'components/comment-button';
+import { CommentCard } from 'components/comment-card';
 
 function ShowTweet() {
   useSetTitle('Tweet', null);
 
   const { currentUser } = useCurrentUser();
+  const { modalOn, types } = useModalType();
 
   const { uuid } = useParams();
   const { data, isLoading, isError } = useQuery(
@@ -34,90 +36,101 @@ function ShowTweet() {
     tweet,
     tweet: {
       twitter,
+      comments,
       meta: { likes },
     },
   } = data.data;
 
   return (
-    <article className="py-3">
-      <div className="px-4 border-b border-gray-200">
-        <div className="flex justify-between">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-              <Avatar
-                size="lg"
-                src={twitter.avatar}
-                alt={twitter.twitterHandle}
+    <div>
+      <article className="py-3">
+        <div className="px-4 border-b border-gray-200">
+          <div className="flex justify-between">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <Avatar
+                  size="lg"
+                  src={twitter.avatar}
+                  alt={twitter.twitterHandle}
+                />
+              </div>
+              <div className="ml-3">
+                <TextButton
+                  to={`/${twitter.twitterHandle}`}
+                  color="black"
+                  size="sm"
+                  className="relative font-bold"
+                >
+                  {twitter.name || a.DEFAULT_NAME}
+                </TextButton>
+                <span className="block text-sm leading-5 text-gray-500">
+                  @{twitter.twitterHandle}
+                </span>
+              </div>
+            </div>
+            {twitter.id === currentUser.id && (
+              <div>
+                <TweetCardOption tweetID={tweet.id} redirect={true} />
+              </div>
+            )}
+          </div>
+          <section className="mt-4">
+            <p className="text-xl leading-8">{tweet.body}</p>
+            {tweet.image && (
+              <figure className="mt-3">
+                <AspectRatio src={tweet.image} alt="Attachment" ratio="3:2" />
+              </figure>
+            )}
+            <div className="flex items-center py-3 text-sm text-gray-500 border-b border-gray-200">
+              <span>{time12format(tweet.createdAt)}</span>
+              <span className="mx-1">&middot;</span>
+              <span>{detailedDate(tweet.createdAt)}</span>
+            </div>
+            <TweetStatistics likes={likes} tweetID={tweet.id} />
+            <div className="flex items-center justify-between w-full max-w-lg py-1 mx-auto">
+              <CommentButton
+                size="sm"
+                showCount={false}
+                onClick={() => {
+                  modalOn({
+                    modalType: types.CREATE_COMMENT_ON_TWEET,
+                    modalProps: {
+                      tweetID: tweet.id,
+                      twitterName: twitter.name,
+                      twitterTwitterHandle: twitter.twitterHandle,
+                      twitterAvatar: twitter.avatar,
+                      tweetBody: tweet.body,
+                      tweetCreatedAt: tweet.createdAt,
+                    },
+                  });
+                }}
+              />
+              <TwitterActionButton
+                icon="refresh"
+                size="md"
+                color="green"
+                className="relative"
+              ></TwitterActionButton>
+              <LikeButton
+                size="md"
+                showCount={false}
+                objectID={tweet.id}
+                status={likes}
+              />
+              <TwitterActionButton
+                icon="upload"
+                size="md"
+                color="teal"
+                className="relative"
               />
             </div>
-            <div className="ml-3">
-              <TextButton
-                to={`/${twitter.twitterHandle}`}
-                color="black"
-                size="sm"
-                className="relative font-bold"
-              >
-                {twitter.name || a.DEFAULT_NAME}
-              </TextButton>
-              <span className="block text-sm leading-5 text-gray-500">
-                @{twitter.twitterHandle}
-              </span>
-            </div>
-          </div>
-          {twitter.id === currentUser.id && (
-            <div>
-              <TweetCardOption tweetID={tweet.id} redirect={true} />
-            </div>
-          )}
+          </section>
         </div>
-        <section className="mt-4">
-          <p className="text-xl leading-8">{tweet.body}</p>
-          {tweet.image && (
-            <figure className="mt-3">
-              <AspectRatio src={tweet.image} alt="Attachment" ratio="3:2" />
-            </figure>
-          )}
-          <div className="flex items-center py-3 text-sm text-gray-500 border-b border-gray-200">
-            <span>{time12format(tweet.createdAt)}</span>
-            <span className="mx-1">&middot;</span>
-            <span>{detailedDate(tweet.createdAt)}</span>
-          </div>
-          <TweetStatistics likes={likes} tweetID={tweet.id} />
-          <div className="flex items-center justify-between w-full max-w-lg py-1 mx-auto">
-            <CommentButton
-              size="sm"
-              showCount={false}
-              tweet={{
-                tweetID: tweet.id,
-                twitterName: twitter.name,
-                twitterTwitterHandle: twitter.twitterHandle,
-                twitterAvatar: twitter.avatar,
-                tweetBody: tweet.body,
-                tweetCreatedAt: tweet.createdAt,
-              }}
-            />
-            <TwitterActionButton
-              icon="refresh"
-              size="md"
-              color="green"
-              className="relative"
-            ></TwitterActionButton>
-            <LikeButton
-              size="md"
-              showCount={false}
-              objectID={tweet.id}
-              status={likes}
-            />
-            <TwitterActionButton
-              icon="upload"
-              size="md"
-              color="teal"
-              className="relative"
-            />
-          </div>
-        </section>
-      </div>
-    </article>
+      </article>
+      {comments.map((comment) => (
+        <CommentCard key={comment.id} comment={comment} />
+      ))}
+    </div>
   );
 }
 
