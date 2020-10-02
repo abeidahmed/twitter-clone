2e982fdc07cabf28fe91a435309d07fc5d7caa2d
abeidahmed <abeidahmed92@gmail.com>
2020-10-02 15:38:12 +0530

    list all the bookmarks

--- client/src/pages/bookmark.js
+++ client/src/pages/bookmark.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useSetTitle } from 'store/page-title';
+import { useCurrentUser } from 'store/current-user';
+import { allBookmarks } from 'api/all-bookmarks';
+import * as q from 'shared/query-key';
+import { TwitterCard } from 'components/twitter-card';
+import { CommentCard } from 'components/comment-card';
+import { Spinner } from 'components/spinner';
 
 function Bookmark() {
+  const { currentUser } = useCurrentUser();
+  useSetTitle('Bookmarks', `@${currentUser.twitterHandle}`);
+
+  const {
+    data: { data: { bookmarks } = {} } = {},
+    isLoading,
+    isError,
+  } = useQuery(q.ALL_BOOKMARKS, allBookmarks);
+
+  if (isLoading || isError) return <Spinner />;
+
   return (
-    <div>
-      <h1>hello from bookmark</h1>
-    </div>
+    <main>
+      {bookmarks.map((bookmark) =>
+        bookmark.type === 'Tweet' ? (
+          <TwitterCard key={bookmark.id} tweet={bookmark} />
+        ) : (
+          <CommentCard key={bookmark.id} comment={bookmark} />
+        )
+      )}
+    </main>
   );
 }
 
