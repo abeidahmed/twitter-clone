b042b5528a94db15b1bbe57863c75d8d8f04757d
abeidahmed <abeidahmed92@gmail.com>
2020-10-02 10:13:00 +0530

    modify to accept comment

--- client/src/components/twitter-card.js
+++ client/src/components/twitter-card.js
@@ -13,55 +13,73 @@ import { AspectRatio } from './aspect-ratio';
 import { TweetCardOption } from './tweet-card-option';
 import { LikeButton } from './like-button';
 import { CommentButton } from './comment-button';
+import { CommentCard } from './comment-card';
 
-export function TwitterCard({ tweet, user }) {
+export function TwitterCard({ tweet, user, showComments }) {
   // In user show page, the user details is not listed in the tweet array,
   // and hence twitter = user.
   const { id, uuid, body, createdAt, image, twitter = user } = tweet;
   const { currentUser } = useCurrentUser();
 
   return (
-    <CardContainer
-      to={`/${twitter.twitterHandle}/status/${uuid}`}
-      bordered={true}
-    >
-      <div className="flex-shrink-0">
-        <Avatar size="md" src={twitter.avatar} alt="" />
-      </div>
-      <div className="flex flex-col flex-1">
-        <div>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <TextButton
-                to={`/${twitter.twitterHandle}`}
-                color="black"
-                size="sm"
-                className="relative font-bold"
-              >
-                {twitter.name || a.DEFAULT_NAME}
-              </TextButton>
-              <span className="pl-2 text-sm leading-5 text-gray-500">
-                @{twitter.twitterHandle}
-              </span>
-              <span className="mx-1">&middot;</span>
-              <span className="text-sm leading-5 text-gray-500">
-                {withPartialMonth(createdAt)}
-              </span>
-            </div>
-            {twitter.id === currentUser.id && <TweetCardOption tweetID={id} />}
+    <>
+      <CardContainer
+        to={`/${twitter.twitterHandle}/status/${uuid}`}
+        bordered={!showComments}
+      >
+        <div className="flex flex-col items-center">
+          <div className="flex-shrink-0">
+            <Avatar size="md" src={twitter.avatar} alt="" />
           </div>
-          <div className="mt-2">
-            <p className="text-gray-600">{body}</p>
-          </div>
-          {image && (
-            <figure className="mt-3">
-              <AspectRatio src={image} alt="attachment" ratio="16:9" />
-            </figure>
+          {showComments && (
+            <div className="flex-1 w-0.5 py-4 my-2 bg-gray-300"></div>
           )}
         </div>
-        <CardActionButtons tweet={tweet} user={user} />
-      </div>
-    </CardContainer>
+        <div className="flex flex-col flex-1">
+          <div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <TextButton
+                  to={`/${twitter.twitterHandle}`}
+                  color="black"
+                  size="sm"
+                  className="relative font-bold"
+                >
+                  {twitter.name || a.DEFAULT_NAME}
+                </TextButton>
+                <span className="pl-2 text-sm leading-5 text-gray-500">
+                  @{twitter.twitterHandle}
+                </span>
+                <span className="mx-1">&middot;</span>
+                <span className="text-sm leading-5 text-gray-500">
+                  {withPartialMonth(createdAt)}
+                </span>
+              </div>
+              {twitter.id === currentUser.id && (
+                <TweetCardOption tweetID={id} />
+              )}
+            </div>
+            <div className="mt-2">
+              <p className="text-gray-600">{body}</p>
+            </div>
+            {image && (
+              <figure className="mt-3">
+                <AspectRatio src={image} alt="attachment" ratio="16:9" />
+              </figure>
+            )}
+          </div>
+          <CardActionButtons tweet={tweet} user={user} />
+        </div>
+      </CardContainer>
+      {showComments &&
+        tweet.comments.map((comment) => (
+          <CommentCard
+            key={comment.id}
+            comment={comment}
+            twitterID={twitter.id}
+          />
+        ))}
+    </>
   );
 }
 
