2bde47c17640549e5a4205ebe841fdf148c9843f
abeidahmed <abeidahmed92@gmail.com>
2020-10-02 15:58:38 +0530

    update api to accept user_id

--- client/src/api/all-bookmarks.js
+++ client/src/api/all-bookmarks.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { header } from 'middleware/header';
 
-export async function allBookmarks(key) {
-  return await axios.get('/bookmarks', header());
+export async function allBookmarks(key, { user_id }) {
+  return await axios.get(`/users/${user_id}/bookmarks`, header());
 }
