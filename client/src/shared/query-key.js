ac3e7c4f855f7444bab7e78b5ab5d11692f51d1b
abeidahmed <abeidahmed92@gmail.com>
2020-10-02 10:13:21 +0530

    add user commented tweet query key

--- client/src/shared/query-key.js
+++ client/src/shared/query-key.js
@@ -4,6 +4,7 @@ export const ALL_TWEETS = 'ALL_TWEETS';
 export const ALL_USERS = 'ALL_USERS';
 export const USER_MEDIA_TWEETS = 'USER_MEDIA_TWEETS';
 export const USER_LIKED_TWEETS = 'USER_LIKED_TWEETS';
+export const USER_COMMENTED_TWEETS = 'USER_COMMENTED_TWEETS';
 export const SHOW_USER = 'SHOW_USER';
 export const SHOW_TWEET = 'SHOW_TWEET';
 export const ALL_TWEET_LIKERS = 'ALL_TWEET_LIKERS';
