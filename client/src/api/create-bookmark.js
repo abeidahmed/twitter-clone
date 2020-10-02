cbc0a63088df1f98492c7666460a2efafc972459
abeidahmed <abeidahmed92@gmail.com>
2020-10-02 18:35:37 +0530

    make create bookmark api

---
+++ client/src/api/create-bookmark.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { header } from 'middleware/header';
+
+export async function bookmarkTweet({ tweetID }) {
+  return await axios.post(`/tweets/${tweetID}/bookmarks`, null, header());
+}
+
+export async function bookmarkComment({ commentID }) {
+  return await axios.post(`/comments/${commentID}/bookmarks`, null, header());
+}
