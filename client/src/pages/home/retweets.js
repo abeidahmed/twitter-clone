a724515144bf681fffe1f3d76cb720f879c73b9a
abeidahmed <abeidahmed92@gmail.com>
2020-10-04 21:28:34 +0530

    loop over retweets

--- client/src/pages/home/retweets.js
+++ client/src/pages/home/retweets.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { RetweetCard } from 'components/retweet-card';
+import { useQuery } from 'react-query';
+import { allRetweets } from 'api/all-retweets';
+import { RetweetCollection } from 'components/retweet-card';
+import { Spinner } from 'components/spinner';
 
 function Retweets() {
+  const {
+    data: { data: { retweets } = {} } = {},
+    isLoading,
+    isError,
+  } = useQuery('allRetweets', allRetweets);
+
   return (
-    <section>
-      <RetweetCard />
-    </section>
+    <>
+      {isLoading || isError ? (
+        <Spinner />
+      ) : (
+        <section>
+          {retweets.map((tweet) => (
+            <RetweetCollection key={tweet.id} tweet={tweet} />
+          ))}
+        </section>
+      )}
+    </>
   );
 }
 
