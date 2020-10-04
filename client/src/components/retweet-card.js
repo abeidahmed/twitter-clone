7fb6d0fff8370e708b3bfa8a732c495c2564bc67
abeidahmed <abeidahmed92@gmail.com>
2020-10-04 21:29:51 +0530

    render retweets

--- client/src/components/retweet-card.js
+++ client/src/components/retweet-card.js
@@ -7,22 +7,39 @@ import { Avatar } from './avatar';
 import { LikeButton } from './like-button';
 import { CommentButton } from './comment-button';
 import { AspectRatio } from './aspect-ratio';
+import { withPartialMonth } from 'utils/date-time';
+import { UserHoverableCard } from './user-hoverable-card';
 
-export function RetweetCard() {
+export function RetweetCollection({ tweet }) {
+  const { body } = tweet;
+
+  if (body) {
+    return <QuoteTweetCard tweet={tweet} />;
+  }
+  return <RetweetCard tweet={tweet} />;
+}
+
+function RetweetCard({ tweet }) {
+  const { retweeter, retweetable } = tweet;
+  const { nestedTweet } = retweetable;
+
   return (
     <CardContainer to="/" bordered={true}>
-      <Retweeter name="Yukhiro Matsumoto Retweeted" linkTo="/" />
+      <Retweeter user={retweeter} />
       <div className="flex space-x-3">
         <div className="flex-shrink-0">
-          <Avatar size="md" src="" alt="" />
+          <Avatar
+            size="md"
+            src={retweetable.twitter.avatar}
+            alt={retweetable.twitter.twitterHandle}
+          />
         </div>
         <div className="flex flex-col flex-1">
           <div>
             <div className="flex items-center justify-between">
               <UserDetail
-                name="Ujin Tamang"
-                twitterHandle="ujinmama"
-                date="26 Sep"
+                user={retweetable.twitter}
+                date={withPartialMonth(retweetable.createdAt)}
               />
               <div className="relative">
                 <IconButton size="sm" color="primary-text">
@@ -31,87 +48,176 @@ export function RetweetCard() {
               </div>
             </div>
             <div>
-              <p className="text-gray-600">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Architecto, distinctio. Lorem ipsum dolor sit amet consectetur,
-                adipisicing elit. Maiores expedita culpa tenetur iure.
-              </p>
+              <p className="text-gray-600">{retweetable.body}</p>
+              {nestedTweet && (
+                <section className="relative p-3 mt-2 transition duration-150 ease-in-out border border-gray-200 rounded-lg hover:bg-gray-200">
+                  <Link to="/" className="absolute inset-0"></Link>
+                  <div className="flex items-center space-x-2">
+                    <Avatar
+                      size="xs"
+                      src={nestedTweet.twitter.avatar}
+                      alt={nestedTweet.twitter.twitterHandle}
+                    />
+                    <UserDetail
+                      user={nestedTweet.twitter}
+                      date={withPartialMonth(nestedTweet.createdAt)}
+                    />
+                  </div>
+                  <div>
+                    <p className="text-gray-600">{nestedTweet.body}</p>
+                  </div>
+                  {nestedTweet.image && (
+                    <figure className="mt-3">
+                      <AspectRatio
+                        src="https://images.unsplash.com/photo-1552435053-01c010307582?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80"
+                        alt="attachment"
+                        ratio="16:9"
+                      />
+                    </figure>
+                  )}
+                </section>
+              )}
+              {retweetable.image && (
+                <figure className="mt-3">
+                  <AspectRatio
+                    src="https://images.unsplash.com/photo-1552435053-01c010307582?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80"
+                    alt="attachment"
+                    ratio="16:9"
+                  />
+                </figure>
+              )}
             </div>
-            <section className="relative p-3 mt-2 transition duration-150 ease-in-out border border-gray-200 rounded-lg hover:bg-gray-200">
-              <Link to="/" className="absolute inset-0"></Link>
-              <div className="flex items-center space-x-2">
-                <Avatar size="xs" src="" alt="" />
-                <UserDetail
-                  name="Tashy Sherpa"
-                  twitterHandle="tashymams"
-                  date="31 Oct"
-                />
-              </div>
-              <div>
-                <p className="text-gray-600">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Sunt, placeat?
-                </p>
-              </div>
-              <figure className="mt-3">
-                <AspectRatio
-                  src="https://images.unsplash.com/photo-1552435053-01c010307582?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80"
-                  alt="attachment"
-                  ratio="16:9"
-                />
-              </figure>
-            </section>
           </div>
-          <div className="flex items-center justify-between w-full max-w-md mt-1 -ml-2">
-            <CommentButton size="sm" showCount={true} count={2} />
-            <TwitterActionButton
-              icon="refresh"
-              size="sm"
-              color="green"
-              className="relative"
-            >
-              4
-            </TwitterActionButton>
-            <LikeButton
-              size="sm"
-              showCount={true}
-              status={{ isLiked: true, totalLikes: 41 }}
-            />
-            <TwitterActionButton
-              icon="upload"
-              size="sm"
-              color="teal"
-              className="relative"
-            />
-          </div>
+          <ActionButtons />
         </div>
       </div>
     </CardContainer>
   );
 }
 
-function Retweeter({ name, linkTo }) {
+function QuoteTweetCard({ tweet }) {
+  const { body, createdAt, retweetable, retweeter } = tweet;
+
   return (
-    <p className="relative flex items-center mb-1 space-x-3 text-xs text-gray-500 ml-7">
-      <Icon icon="refresh" className="w-3 h-3" />
-      <Link to={linkTo} className="hover:underline">
-        {name}
-      </Link>
-    </p>
+    <CardContainer to="/" bordered={true}>
+      <div className="flex space-x-3">
+        <div className="flex-shrink-0">
+          <Avatar
+            size="md"
+            src={retweeter.avatar}
+            alt={retweeter.twitterHandle}
+          />
+        </div>
+        <div className="flex flex-col flex-1">
+          <div>
+            <div className="flex items-center justify-between">
+              <UserDetail user={retweeter} date={withPartialMonth(createdAt)} />
+              <div className="relative">
+                <IconButton size="sm" color="primary-text">
+                  <Icon icon="chevron-down" className="w-4 h-4" />
+                </IconButton>
+              </div>
+            </div>
+            <div>
+              <p className="text-gray-600">{body}</p>
+            </div>
+            <section className="relative p-3 mt-2 transition duration-150 ease-in-out border border-gray-200 rounded-lg hover:bg-gray-200">
+              <Link to="/" className="absolute inset-0"></Link>
+              <div className="flex items-center space-x-2">
+                <Avatar
+                  size="xs"
+                  src={retweetable.twitter.avatar}
+                  alt={retweetable.twitter.twitterHandle}
+                />
+                <UserDetail
+                  user={retweetable.twitter}
+                  date={withPartialMonth(retweetable.createdAt)}
+                />
+              </div>
+              <div>
+                <p className="text-gray-600">{retweetable.body}</p>
+              </div>
+              {retweetable.image && (
+                <figure className="mt-3">
+                  <AspectRatio
+                    src="https://images.unsplash.com/photo-1552435053-01c010307582?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80"
+                    alt="attachment"
+                    ratio="16:9"
+                  />
+                </figure>
+              )}
+            </section>
+          </div>
+          <ActionButtons />
+        </div>
+      </div>
+    </CardContainer>
   );
 }
 
-function UserDetail({ name, twitterHandle, date, linkTo }) {
+function ActionButtons() {
+  return (
+    <div className="flex items-center justify-between w-full max-w-md mt-1 -ml-2">
+      <CommentButton size="sm" showCount={true} count={2} />
+      <TwitterActionButton
+        icon="refresh"
+        size="sm"
+        color="green"
+        className="relative"
+      >
+        4
+      </TwitterActionButton>
+      <LikeButton
+        size="sm"
+        showCount={true}
+        status={{ isLiked: true, totalLikes: 41 }}
+      />
+      <TwitterActionButton
+        icon="upload"
+        size="sm"
+        color="teal"
+        className="relative"
+      />
+    </div>
+  );
+}
+
+function Retweeter({ user }) {
+  const { name, twitterHandle } = user;
+
+  return (
+    <div className="relative inline-flex items-center mb-1 space-x-3 text-xs text-gray-500 ml-7">
+      <Icon icon="refresh" className="w-3 h-3" />
+      <UserHoverableCard
+        hoverTo={
+          <Link to={`/users/${twitterHandle}`} className="hover:underline">
+            {`${name} Retweeted`}
+          </Link>
+        }
+        user={user}
+      />
+    </div>
+  );
+}
+
+function UserDetail({ user, date }) {
+  const { name, twitterHandle } = user;
+
   return (
     <div className="flex items-center">
-      <TextButton
-        to={linkTo}
-        color="black"
-        size="sm"
-        className="relative font-bold"
-      >
-        {name}
-      </TextButton>
+      <UserHoverableCard
+        hoverTo={
+          <TextButton
+            to={`/users/${twitterHandle}`}
+            color="black"
+            size="sm"
+            className="relative font-bold"
+          >
+            {name}
+          </TextButton>
+        }
+        user={user}
+      />
       <span className="pl-2 text-sm leading-5 text-gray-500">
         @{twitterHandle}
       </span>
