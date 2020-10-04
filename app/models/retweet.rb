930dd0f5f0ade940900930403391adfc94ae57fd
abeidahmed <abeidahmed92@gmail.com>
2020-10-04 12:12:37 +0530

    delete retweetable association when deleting retweets

--- app/models/retweet.rb
+++ app/models/retweet.rb
@@ -5,7 +5,7 @@ class Retweet < ApplicationRecord
 
   belongs_to :retweetable, polymorphic: true
   belongs_to :user
-  has_many :retweets, as: :retweetable
+  has_many :retweets, as: :retweetable, dependent: :destroy
 
   validates_length_of :body, maximum: 240
 end
