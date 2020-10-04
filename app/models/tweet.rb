e55260abd8c6f26cc0ff89abbb4e3a1946d438c5
abeidahmed <abeidahmed92@gmail.com>
2020-10-04 21:48:54 +0530

    shorten var from current_user to usre

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -19,7 +19,7 @@ class Tweet < ApplicationRecord
 
   scope :own_tweets, ->(user) { where(user_id: user.id) }
   scope :from_people_you_follow, ->(user) { where(user_id: user.following.pluck(:id)) }
-  scope :from_people_you_know, ->(current_user) { from_people_you_follow(current_user).or(own_tweets(current_user)) }
+  scope :from_people_you_know, ->(user) { from_people_you_follow(user).or(own_tweets(user)) }
   scope :consist_images, -> { where.not(image: nil) }
   scope :with_comments_from, ->(user) { includes(:comments).where('comments.user_id': user.id) }
 end
