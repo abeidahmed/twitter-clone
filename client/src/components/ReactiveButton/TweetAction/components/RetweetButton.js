04506d0fdbc9180c496876f7712d81a772ae21b9
abeidahmed <abeidahmed92@gmail.com>
2020-10-08 10:32:57 +0530

    allow users to undo tweet retweet

--- client/src/components/ReactiveButton/TweetAction/components/RetweetButton.js
+++ client/src/components/ReactiveButton/TweetAction/components/RetweetButton.js
@@ -1,23 +1,51 @@
 import React, { useState } from 'react';
 import { useRefetchMutation } from 'hooks/useRefetchMutation';
 import { createTweetRetweet } from 'api/create-retweet';
+import { deleteRetweet } from 'api/delete-retweet';
 import * as q from 'shared/queryKey';
 import { RetweetButton, MenuButton } from 'components/Button';
 import { OutsideClickHandler, DropdownContainer } from 'components/Container';
 
 function RetweetBtn({ tweet, showCount }) {
-  const { id } = tweet;
+  const { id, meta: { retweets } = {} } = tweet;
+  const { isRetweeted } = retweets;
   const [menuActive, setMenuActive] = useState(false);
 
-  const [mutate, { isLoading }] = useRefetchMutation(createTweetRetweet, [
-    q.ALL_RETWEETS,
-  ]);
+  const [
+    createRetweet,
+    { isLoading: retweeting },
+  ] = useRefetchMutation(createTweetRetweet, [q.ALL_RETWEETS, q.ALL_TWEETS]);
   async function handleRetweet() {
-    await mutate({
+    await createRetweet({
       tweetID: id,
       body: null,
     });
   }
+
+  const [
+    undoRetweet,
+    { isLoading: deleting },
+  ] = useRefetchMutation(deleteRetweet, [q.ALL_TWEETS]);
+  async function handleUndoRetweet() {
+    await undoRetweet({
+      id,
+    });
+  }
+
+  const links = [
+    {
+      title: isRetweeted ? 'Undo Retweet' : 'Retweet',
+      icon: 'refresh',
+      onClick: isRetweeted ? handleUndoRetweet : handleRetweet,
+      disabled: isRetweeted ? deleting : retweeting,
+    },
+    {
+      title: 'Quote Tweet',
+      icon: 'pencil',
+      // onClick: handleRetweet,
+      // disabled: isLoading,
+    },
+  ];
 
   return (
     <OutsideClickHandler
@@ -25,24 +53,24 @@ function RetweetBtn({ tweet, showCount }) {
       className="relative flex items-center justify-center"
     >
       <RetweetButton
-        status={{ isRetweeted: false, totalRetweets: 4 }}
+        status={retweets}
         showCount={showCount}
         size="sm"
         onClick={() => setMenuActive(!menuActive)}
       />
       <DropdownContainer isActive={menuActive}>
-        <MenuButton
-          size="md"
-          icon="refresh"
-          variant="menu"
-          onClick={handleRetweet}
-          disabled={isLoading}
-        >
-          Retweet
-        </MenuButton>
-        <MenuButton size="md" icon="pencil" variant="menu">
-          Quote Tweet
-        </MenuButton>
+        {links.map((link) => (
+          <MenuButton
+            key={link.title}
+            size="md"
+            icon={link.icon}
+            variant="menu"
+            onClick={link.onClick ? link.onClick : null}
+            disabled={link.disabled && link.disabled}
+          >
+            {link.title}
+          </MenuButton>
+        ))}
       </DropdownContainer>
     </OutsideClickHandler>
   );
